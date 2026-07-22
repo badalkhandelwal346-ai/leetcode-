e77a556b09245c9c6808339d24f45c8aa98cd691
Badal khandelwal <badalkhandelwal346@gmail.com>
2026-07-22 13:21:31 +0530

    Time: 3 ms (31.89%), Space: 19.2 MB (65.41%) - LeetHub

---
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSmallestString(self, s: str) -> str:
+        a=s
+        s=list(s)
+        for i in range(len(s)-1):
+            if (int(s[i])%2==0 and int(s[i+1])%2==0) or (int(s[i])%2!=0 and int(s[i+1])%2!=0):
+                if int(s[i])>int(s[i+1]):
+                    s[i],s[i+1]=s[i+1],s[i]
+                    a=min(a,"".join(s))
+                    s[i],s[i+1]=s[i+1],s[i]
+        return str(a)            
+
+
+                    
+
+        
